b7caabfa3e1aa7b796a525cb59fe4d27294d3a0e
Amy Grace <54113654+00p513-dev@users.noreply.github.com>
2020-08-08 14:37:11 +0100

    (MIUI 9) Make provisionned on MIUI 12 too (#2)
    Co-authored-by: 4PERTURE <38117317+4PERTURE@users.noreply.github.com>

--- roms/9/MIUI/make.sh
+++ roms/9/MIUI/make.sh
@@ -53,3 +53,21 @@ if grep -iq ro.miui.ui.version.name=V11 $1/build.prop;then
     sed -i 's/boot_completed=1/boot_completed=1\n    # force \"provision and usersetup\" to be true on boot complete\n    exec \- root \-\- \/system\/bin\/sh \-c \"settings put secure user_setup_complete 1 \&\& settings put global device_provisioned 1\"/' $1/etc/init/boot.init.miui.rc
     
 fi
+
+# workaround for MIUI v12 on some devices
+
+if grep -iq ro.miui.ui.version.name=V12 $1/build.prop;then
+
+    echo "MIUI V12 Detected, make device provisioned on init"
+
+    # device is already provisioned on init
+    # removing these apps
+    rm -rf $1/priv-app/Provision
+    rm -rf $1/priv-app/dpmserviceapp
+
+    # add settings put secure user_setup_complete 1
+    # add settings put global device_provisioned 1
+    # on boot.init.miui.rc to set these variables to true on boot
+    sed -i 's/boot_completed=1/boot_completed=1\n    # force \"provision and usersetup\" to be true on boot complete\n    exec \- root \-\- \/system\/bin\/sh \-c \"settings put secure user_setup_complete 1 \&\& settings put global device_provisioned 1\"/' $1/etc/init/boot.init.miui.rc
+
+fi
